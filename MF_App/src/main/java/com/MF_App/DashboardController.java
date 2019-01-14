3c353191af31231908f6e0eb4f47e5102602fa8a
jimsweeney <jimsweeney@172.25.150.115>
2019-01-14 15:03:14 -0500

    Addition of websecurity

--- MF_App/src/main/java/com/MF_App/DashboardController.java
+++ MF_App/src/main/java/com/MF_App/DashboardController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class DashboardController 
 {
-	@RequestMapping(value="/", method=RequestMethod.GET)
 	
+	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String rootView()
 	{
 		return "index";
