0ace2703b4c9d2f38634feedd0b0fa016c41d1ac
jimsweeney <jimsweeney@172.25.150.120>
2019-02-20 12:05:34 -0500

    Beginning stages of leveraging a database for user authentication

--- MF_App/src/main/java/com/MF_App/security/WebSecurityConfig.java
+++ MF_App/src/main/java/com/MF_App/security/WebSecurityConfig.java
@@ -1,10 +1,12 @@
 package com.MF_App.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -12,6 +14,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 {
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+
 	@Bean
 	public PasswordEncoder getPasswordEncoder()
 	{
@@ -22,11 +27,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		auth.inMemoryAuthentication()
-			.passwordEncoder(getPasswordEncoder())
-			.withUser("jsweeney76")
-			.password(getPasswordEncoder().encode("123456"))
-			.roles("USER");
+		auth
+		
+			.userDetailsService(userDetailsService)
+			.passwordEncoder(getPasswordEncoder());
+			
+		
+//		auth.inMemoryAuthentication()
+//			.passwordEncoder(getPasswordEncoder())
+//			.withUser("jsweeney76")
+//			.password(getPasswordEncoder().encode("123456"))
+//			.roles("USER");
 	}
 	
 	@Override
