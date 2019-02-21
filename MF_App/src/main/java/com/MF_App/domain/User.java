d750d31ead63034df81efd9e806b7e2d95eec897
jimsweeney <jimsweeney@192.168.1.105>
2019-02-20 21:37:05 -0500

    Additional implementation of spring security

--- MF_App/src/main/java/com/MF_App/domain/User.java
+++ MF_App/src/main/java/com/MF_App/domain/User.java
@@ -1,11 +1,19 @@
 package com.MF_App.domain;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.MF_App.security.Authority;
 
 @Entity
 @Table(name="users")
@@ -19,6 +27,7 @@ public class User
 	private String team;
 	private String username;
 	private String password;
+	private Set<Authority> authorities = new HashSet<>();
 	
 	@Id 
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -90,6 +99,17 @@ public class User
 	public void setPassword(String password) 
 	{
 		this.password = password;
+	}
+
+	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.EAGER, mappedBy="user")
+	public Set<Authority> getAuthorities() 
+	{
+		return authorities;
+	}
+
+	public void setAuthorities(Set<Authority> authorities) 
+	{
+		this.authorities = authorities;
 	}
 	
 
